dcdbf90a5781a6446c4a2e57adc862461621dc00
maulikvarshney <varshneymaulik05@gmail.com>
2025-11-15 01:36:53 +0530

    Vite

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,9 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
+// Change the base path to match your repo name
 export default defineConfig({
   plugins: [react()],
-  server: { port: 5173 }
-})
+  base: "/Dr-GauravVarshney/",
+  server: { port: 5173 },
+});
