242e6d5dac4d2d731b8801ab692e228dc7c048e3
maulikvarshney <varshneymaulik05@gmail.com>
2025-11-15 21:34:18 +0530

    x

--- src/main.tsx
+++ src/main.tsx
@@ -4,8 +4,7 @@ import { HashRouter as Router } from 'react-router-dom'
 import App from './App'
 import './index.css'
 
-// ✅ place console.log outside the JSX (not inside <React.StrictMode>)
-console.log("Base Path:", import.meta.env.BASE_URL);
+console.log("Base Path:", import.meta.env.BASE_URL)
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
     <React.StrictMode>
